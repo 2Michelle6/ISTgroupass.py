c4b5c636b806ea9cfb10e61b65c57bb51f3cccb2
2Michelle6 <mwangimichelle26@gmail.com>
2024-07-31 10:22:21 +0300

    Update assignmentII.py
    Write a Python program to calculate the roots of a quadratic equation using the quadratic formula. The program prompts the user to enter the coefficients A, B and C and calculates the two roots. If the roots are equal only one root is displayed. Hint:
    Root1 = (-B + B  4  A  C
    2
    ) / (2.0 A)
    Root2 = (-B - B  4  A  C
    2
    ) / (2.0 A)

--- assignmentII.py
+++ assignmentII.py
@@ -1,1 +1,13 @@
 print ("Tracer")
+import math
+
+A=int(input("Enter coefficient A: "))
+B=int(input("Enter coefficient B: "))
+C=int(input("Enter coefficient C: "))
+root= (B**2-4*A*C)
+
+
+if root > 0:
+    Root1 = (-B+math.sqrt(root)/(2.0*A))
+    Root2 = (-B-math.sqrt(root)/(2.0*A))
+    print(f"root1 is: {Root1} root 2 is {Root2}")
